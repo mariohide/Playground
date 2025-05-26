e5801cc807ff683f7aad99e0d2296fe49ec53f28
kongdeli <kongdeliandroid@gmail.com>
2025-05-26 14:40:53 +0800

    remove unused codes

--- app/src/main/java/code/mario/playground/I18nScreen.kt
+++ app/src/main/java/code/mario/playground/I18nScreen.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.material3.Text
-import androidx.compose.material3.Typography
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
@@ -28,11 +27,7 @@ fun I18nScreen() {
             .fillMaxSize()
             .systemBarsPadding()
     ) {
-        val imgList: List<Any> = listOf(R.drawable.pic_i18n)
         val context = LocalContext.current
-        LaunchedEffect(imgList) {
-            Log.d(TAG, "I18nScreen: img list changed, $imgList")
-        }
         LaunchedEffect(context) {
             Log.d(TAG, "I18nScreen: context changed, $context")
         }
@@ -45,14 +40,6 @@ fun I18nScreen() {
         AsyncImage(
             model = ImageRequest.Builder(context)
                 .data(R.drawable.pic_i18n)
-                .memoryCachePolicy(CachePolicy.DISABLED)
-                .diskCachePolicy(CachePolicy.DISABLED)
-                .build(),
-            contentDescription = null
-        )
-        AsyncImage(
-            model = ImageRequest.Builder(context)
-                .data(imgList.first())
                 .memoryCachePolicy(CachePolicy.DISABLED)
                 .diskCachePolicy(CachePolicy.DISABLED)
                 .build(),
